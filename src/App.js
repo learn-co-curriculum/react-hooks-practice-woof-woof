cafba9551fb771ab6a2cca3a7e435150154772ec
schazbot <charlotte.pearce1984@gmail.com>
2020-04-21 14:15:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="filter-div">
+        <button id="good-dog-filter">Filter good dogs: OFF</button>
+      </div>
+      <div id="dog-bar">
+
+      </div>
+      <div id="dog-summary-container">
+        <h1>DOGGO:</h1>
+        <div id="dog-info">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
